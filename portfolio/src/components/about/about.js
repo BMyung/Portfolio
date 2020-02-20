23dbfd2a560c3abcf37bd1485cc393505bfe4ca6
Bryan Myung <bryanmyung@gmail.com>
2020-02-19 21:05:56 -0800

    picture update

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -4,7 +4,7 @@ export default class About extends Component {
     render(){
         return (
             <div className="page about" id="about">
-                <img src="img/bryan-myung_270x300.jpg" alt="bryan myung headshot"/>
+                <img src="img/bmprofile.jpg" alt="bryan myung headshot"/>
                 <div className="desc"> <h1>About Me</h1>
                     <p>Hi, I'm Bryan, a full stack web developer based in Los Angeles, California. I have experience with JavaScript, Python, Java, and I am always excited to learn more. Let's build something together!</p>
                 </div>
